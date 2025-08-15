83cb8c6ded46061e1c358542b57ae4f45b41012a
cryswerton <cryswertons@gmail.com>
2025-08-15 15:21:15 +0100

    added table property to all queries

--- src/private/models/Link.php
+++ src/private/models/Link.php
@@ -15,26 +15,26 @@ class Link {
     }
 
     public function update($id, $url) {
-        $stmt = $this->pdo->prepare("UPDATE links SET url = :url WHERE id = :id");
+        $stmt = $this->pdo->prepare("UPDATE {$this->table} SET url = :url WHERE id = :id");
         $stmt->bindParam(':url', $url);
         $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
 
     public function getAll() {
-        $stmt = $this->pdo->query("SELECT * FROM links");
+        $stmt = $this->pdo->query("SELECT * FROM {$this->table}");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getById($id) {
-        $stmt = $this->pdo->prepare("SELECT * FROM links WHERE id = :id");
+        $stmt = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function delete($id) {
-        $stmt = $this->pdo->prepare("DELETE FROM links WHERE id = :id");
+        $stmt = $this->pdo->prepare("DELETE FROM {$this->table} WHERE id = :id");
         $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
